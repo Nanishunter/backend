7b38b3fc566acef5a105e52a29ed61eb6650f858
1701542-Saimia <anttiyleinen@hotmail.com>
2019-01-14 08:55:17 +0200

    Added PersonRepository and IPersonRepository

---
+++ WebApiExample/WebApiExample/Repositories/PersonRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiExample.Models;
+
+namespace WebApiExample.Repositories
+{
+    public class PersonRepository: IPersonRepository
+    {
+        public Person Create(Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Person> Read()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Read(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Person Update(int id, Person person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
